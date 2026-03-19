9b815b6e54919da489b51a000fe1d9267f477ae4
kirillmalafeev27-ai <kirillmalafeev27@gmail.com>
2026-03-19 13:28:24 +0300

    Add files via upload

---
+++ knot-words-site/src/data/fallback_levels.js
@@ -0,0 +1,15 @@
+import { A1_LEVELS } from "./levels/a1_levels.js";
+import { A2_LEVELS } from "./levels/a2_levels.js";
+import { B1_LEVELS } from "./levels/b1_levels.js";
+import { B2_LEVELS } from "./levels/b2_levels.js";
+
+export const FALLBACK_LEVELS = Object.freeze({
+  A1: A1_LEVELS,
+  A2: A2_LEVELS,
+  B1: B1_LEVELS,
+  B2: B2_LEVELS,
+});
+
+export function getFallbackLevelPack(level) {
+  return FALLBACK_LEVELS[level] ?? A1_LEVELS;
+}
